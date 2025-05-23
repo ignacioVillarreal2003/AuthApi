154e5d980169a7e5d5bf747bb44432380d299234
Ignacio <ignaciovillarreal20031231@gmail.com>
2025-05-23 09:59:55 -0300

    update docker files

--- src/main/java/com/api/authapi/config/JwtService.java
+++ src/main/java/com/api/authapi/config/JwtService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
